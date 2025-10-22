2f7ebf06788d46824db8691207629742c0343eb2
Thota-bharat999 <bharatmanithota@gmail.com>
2025-10-22 17:04:32 +0530

    ...

--- offer_letter/utils/pdfGenerator.js
+++ offer_letter/utils/pdfGenerator.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const ejs = require("ejs");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const fs = require("fs");
 
 const generateOfferPDF = async (offerData) => {
