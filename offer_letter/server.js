0811c192fc6cf148925119e9eba0b37efd5c298c
Thota-bharat999 <bharatmanithota@gmail.com>
2025-10-22 13:34:19 +0530

    port issue

--- offer_letter/server.js
+++ offer_letter/server.js
@@ -31,5 +31,5 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => res.send("Offer Letter Generator API is running..."));
-  const PORT=process.env.PORT || 3000;
+  const PORT=process.env.PORT || 5000;
   app.listen(PORT,()=>console.log(`Server Running on ${PORT}`))
