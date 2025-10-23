ece5c649ab8825f2d509ca710e69447b950bec75
Thota-bharat999 <bharatmanithota@gmail.com>
2025-10-23 19:08:09 +0530

    ...

--- offer_letter/server.js
+++ offer_letter/server.js
@@ -1,19 +1,25 @@
 require("dotenv").config()
 const express=require("express");
+const cors=require("cors");
 const mongoose=require("mongoose")
 const app=express();
 const bodyParser=require("body-parser");
-const cors=require("cors");
+
 app.use(express.json()); 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 const offerRoutes=require('./routes/offerRoutes');
 const companyRoutes=require('./routes/companyRoutes')
+
+
 app.use(
   cors({
-    origin: "*", // or specify: ["http://localhost:3000"]
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001", // your local dev
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 app.use('/assets', express.static(__dirname + '/offer_letter/assets'));
