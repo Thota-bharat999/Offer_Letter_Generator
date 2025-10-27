52f54b63ff48ea76df089770f7cb128e7dbad2b3
Thota-bharat999 <bharatmanithota@gmail.com>
2025-10-27 16:12:07 +0530

    ..

--- offer_letter/utils/pdfGenerator.js
+++ offer_letter/utils/pdfGenerator.js
@@ -127,6 +127,7 @@ const generateOfferPDF = async (offerData) => {
     console.log("🟩 [10] Launching Puppeteer...");
     const browser = await puppeteer.launch({
       headless: true,
+      channel: 'chromium',
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
