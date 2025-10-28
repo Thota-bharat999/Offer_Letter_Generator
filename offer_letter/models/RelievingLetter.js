829caa01147222525c5b36a6726fb9f804be1375
Thota-bharat999 <bharatmanithota@gmail.com>
2025-10-28 12:26:33 +0530

    designation

--- offer_letter/models/RelievingLetter.js
+++ offer_letter/models/RelievingLetter.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 const relivingLetterSchema=new mongoose.Schema({
     employeeName:{type:String,required:true},
-    desiganation:{type:String,required:true},
+    designation:{type:String,required:true},
     employeeId :{type:String,required:true},
     joiningDate:{type:Date,required:true},
     relievingDate:{type:Date,required:true},
